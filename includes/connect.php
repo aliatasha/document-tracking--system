cc0427793837610c603a07422c3aa6505cb0ea92
anizati <shafiq_razman@yahoo.com>
2021-01-19 10:55:22 +0800

    change connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,8 +1,8 @@
 <?php
-	$dbhost = "127.0.0.1"; //Host
-	$dbuser = "root"; //Database user
-	$dbpass = ""; //Database password
-	$dbname = "phpmultiuserlogin"; //Database name
+	$dbhost = "us-cdbr-east-03.cleardb.com"; //Host
+	$dbuser = "b1efb14225fcf0"; //Database user
+	$dbpass = "0c4df2ef"; //Database password
+	$dbname = "heroku_d6af60c381bc17f"; //Database name
 	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname); //Connection
 	mysqli_set_charset($conn,"utf8"); //UTF-8 for Turkish letters
 ?>
